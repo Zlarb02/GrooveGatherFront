086f7e41a8050e827215ca2037cd3f768711c822
Etienne Pogoda <pogoda.etienne@outlook.fr>
2024-06-15 02:17:02 +0200

    handle header and page size match footer size, add scroll in pages, search bar ok, begin search page template

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { SearchBarComponent } from '../../core/search-bar/search-bar.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [],
+  imports: [SearchBarComponent, ReactiveFormsModule, FormsModule],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
 export class SearchComponent {
-
+  onSubmit(searchForm: any) {
+    if (searchForm.valid) {
+      console.table(searchForm.value);
+    }
+  }
 }
