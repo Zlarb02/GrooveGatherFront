99bb37bc31a2313a8d69390a24238b4db50d09d6
Etienne Pogoda <pogoda.etienne@outlook.fr>
2024-06-19 05:57:50 +0200

    continue working on filters for search page

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,35 +1,59 @@
-import { Component, EventEmitter, Output, inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { SearchBarComponent } from '../../core/search-bar/search-bar.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ProjectCardComponent } from '../../core/project-card/project-card.component';
 import { ProjectService } from '../../shared/services/project.service';
 import type { Project } from '../../shared/models/project.model';
 import { CommonModule } from '@angular/common';
 import { NgxPaginationModule } from 'ngx-pagination';
+import type { skillName } from '../../shared/models/skill-name';
+import { SkillNamesList } from '../../shared/models/skill-names-list';
+import { MatAutocomplete, MatOption, MatAutocompleteModule } from '@angular/material/autocomplete';
+import { map, startWith } from 'rxjs';
+import { genresList } from '../../shared/models/genres-list';
+import type { Genre } from '../../shared/models/genre';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [SearchBarComponent, ReactiveFormsModule, FormsModule, ProjectCardComponent, CommonModule, NgxPaginationModule],
+  imports: [SearchBarComponent, ReactiveFormsModule, FormsModule, ProjectCardComponent, CommonModule, NgxPaginationModule, MatAutocomplete, MatOption, MatAutocompleteModule],
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent {
   searchPlaceholder = `Nom de projet, tag ou nom d'utilisateur`;
 
-  filters: string[] = [];
+  filters: (skillName | Genre)[] = ['Guitare 12 cordes', 'Batterie acoustique', 'Basse fretless', 'Pop psychédélique', 'Hip-hop australien'];
+
+  skillsPossibles = SkillNamesList;
+  filteredSkills: string[] = [];
+
+  genresPossibles = ['pop', 'rock']
+  filteredGenres: string[] = [];
+
   isSortedAsc = true; // State for sort order
 
   projects: Project[] = [];
+
+  genres = genresList;
+
+  showMore = false;
+
 
   p = 1;
   itemsPerPage = 2;
   totalItems: number | undefined;
 
+  // Ajout des contrôles de formulaire pour l'autocomplétion
+  skillControl = new FormControl();
+  genreControl = new FormControl();
+
   projectService = inject(ProjectService);
+
 
   ngOnInit(): void {
     this.getProjects();
+    this.setupAutocomplete();
   }
 
   getProjects() {
@@ -37,6 +61,40 @@ export class SearchComponent {
       this.projects = projects;
       this.totalItems = this.projects.length;
     });
+  }
+
+  removeFilter(index: number): void {
+    // Supprime le filtre à l'index spécifié
+    this.filters.splice(index, 1);
+  }
+
+  setupAutocomplete(): void {
+    this.skillControl.valueChanges
+      .pipe(
+        startWith(''),
+        map(value => this._filterSkills(value))
+      )
+      // biome-ignore lint/suspicious/noAssignInExpressions: <explanation>
+      .subscribe(skills => this.filteredSkills = skills);
+
+    this.genreControl.valueChanges
+      .pipe(
+        startWith(''),
+        map(value => this._filterGenres(value))
+      )
+      // biome-ignore lint/suspicious/noAssignInExpressions: <explanation>
+      .subscribe(genres => this.filteredGenres = genres);
+  }
+
+
+  private _filterSkills(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.skillsPossibles.filter(skill => skill.toLowerCase().includes(filterValue));
+  }
+
+  private _filterGenres(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.genresPossibles.filter(genre => genre.toLowerCase().includes(filterValue));
   }
 
 
