62f5fda1608a6a7a878c71e35b900b0a42c68580
Etienne Pogoda <pogoda.etienne@outlook.fr>
2024-06-20 17:33:42 +0200

    work hard with my friends

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { MenuComponent } from '../menu/menu.component';
 import { ProfileMenuComponent } from '../profile-menu/profile-menu.component';
+import { SearchBarComponent } from '../search-bar/search-bar.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MenuComponent, ProfileMenuComponent],
+  imports: [MenuComponent, ProfileMenuComponent, SearchBarComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
