06050de436259dfbe864afd3e3f377b4a4e3b2ae
Etienne Pogoda <pogoda.etienne@outlook.fr>
2024-06-26 15:11:18 +0200

    profil-menu route removed, improve link accecibility (can click on entire li) in the menu comp

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import type { Routes } from '@angular/router';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
 import { LoginComponent } from './pages/auth/login/login.component';
 import { BestPracticesComponent } from './pages/best-practices/best-practices.component';
 import { HomeComponent } from './pages/home/home.component';
@@ -9,8 +10,6 @@ import { CreateProjectComponent } from './pages/project/create-project/create-pr
 import { ProjectDetailComponent } from './pages/project/project-detail/project-detail.component';
 import { SearchComponent } from './pages/search/search.component';
 import { ProfileComponent } from './pages/user/profile/profile.component';
-import { AboutUsComponent } from './pages/about-us/about-us.component';
-import { ProfileMenuComponent } from './core/profile-menu/profile-menu.component';
 
 export const routes: Routes = [
   {
@@ -45,12 +44,6 @@ export const routes: Routes = [
     path: 'profile',
     component: ProfileComponent,
   },
-
-  {
-    path: 'profile-menu',
-    component: ProfileMenuComponent,
-  },
-
   {
     path: 'project/:id',
     component: ProjectDetailComponent,
@@ -67,9 +60,8 @@ export const routes: Routes = [
     path: 'privacy-policy',
     component: PrivacyPolicyComponent,
   },
-
   {
     path: '**',
     redirectTo: '',
-  },
+  }
 ];
