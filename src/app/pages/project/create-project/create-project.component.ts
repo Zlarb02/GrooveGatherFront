1c235bc27af09fe4b4d2d49176bae4dc0aff81ea
StanislasComputerScience <remilabonne@yahoo.fr>
2024-06-27 16:40:01 +0200

    formulaire nouvelle version

--- src/app/pages/project/create-project/create-project.component.ts
+++ src/app/pages/project/create-project/create-project.component.ts
@@ -1,12 +1,68 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-create-project',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './create-project.component.html',
-  styleUrl: './create-project.component.css'
+  styleUrls: ['./create-project.component.css'],
 })
 export class CreateProjectComponent {
+  myForm: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.myForm = this.formBuilder.group({
+      nameProject: ['', Validators.required],
+      nameOwner: ['', Validators.required],
+      genre: ['', Validators.required],
+      description: ['', Validators.required],
+      date: ['', Validators.required],
+      like: ['0'],
+    });
+
+    this.ecouter();
+  }
+
+  onSubmit() {
+    if (this.myForm.valid) {
+      console.log(this.myForm.value);
+      //      const user: User = this.myForm.value;
+      //      console.log(user);
+    } else {
+      console.log('Form is invalid');
+    }
+  }
+
+  // Méthode pour écouter les changements de valeurs du formulaire
+  ecouter() {
+    this.myForm.valueChanges.subscribe((value) => {
+      console.log(value);
+    });
+  }
+
+  update(user: User) {
+    this.myForm.setValue({
+      nameProject: user.nameProject,
+      nameOwner: user.nameOwner,
+      genre: user.genre,
+      description: user.description,
+      date: user.date,
+    });
+  }
+}
+
+// Assurez-vous que la classe User est définie quelque part dans votre code, par exemple :
+interface User {
+  nameProject: string;
+  nameOwner: string;
+  genre: string;
+  description: string;
+  date: string;
 }
