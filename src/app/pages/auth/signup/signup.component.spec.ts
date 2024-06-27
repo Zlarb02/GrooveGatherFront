a03c6039bb8915317bf19a2b79a5a805db0e4989
Etienne Pogoda <pogoda.etienne@outlook.fr>
2024-06-27 13:55:06 +0200

    login signup styles

---
+++ src/app/pages/auth/signup/signup.component.spec.ts
@@ -0,0 +1,23 @@
+import { TestBed, type ComponentFixture } from '@angular/core/testing';
+
+import { SignupComponent } from './signup.component';
+
+describe('SignupComponent', () => {
+  let component: SignupComponent;
+  let fixture: ComponentFixture<SignupComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [SignupComponent]
+    })
+      .compileComponents();
+
+    fixture = TestBed.createComponent(SignupComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
