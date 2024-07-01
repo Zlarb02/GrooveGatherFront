00f3e22845d2200c6187e4f462d5eb6b66edef98
StanislasComputerScience <remilabonne@yahoo.fr>
2024-07-01 08:36:11 +0200

    ajout et modif des compétences utilisées et demandées

--- src/app/pages/project/create-project/create-project.component.ts
+++ src/app/pages/project/create-project/create-project.component.ts
@@ -1,12 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-// biome-ignore lint/style/useImportType: <explanation>
-import {
-  FormBuilder,
-  FormGroup,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
+import { FormBuilder, FormGroup, FormArray, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { genresList } from '../../../shared/models/genres-list';
+import { SkillNamesList } from '../../../shared/models/skill-names-list';
 
 @Component({
   selector: 'app-create-project',
@@ -17,34 +13,64 @@ import {
 })
 export class CreateProjectComponent {
   myForm: FormGroup;
+  genresList = genresList;
+  skillNamesList = SkillNamesList;
 
   constructor(private formBuilder: FormBuilder) {
     this.myForm = this.formBuilder.group({
       nameProject: ['', Validators.required],
       nameOwner: ['', Validators.required],
       genre: ['', Validators.required],
+      usedSkills: this.formBuilder.array([], Validators.required),
+      requestedSkills: this.formBuilder.array([], Validators.required),
       description: ['', Validators.required],
-      date: ['', Validators.required],
       like: ['0'],
     });
 
     this.ecouter();
   }
 
+  get usedSkills(): FormArray {
+    return this.myForm.get('usedSkills') as FormArray;
+  }
+
+  get requestedSkills(): FormArray {
+    return this.myForm.get('requestedSkills') as FormArray;
+  }
+
+  addUsedSkill(event: Event) {
+    const target = event.target as HTMLSelectElement;
+    if (target && target.value && !this.usedSkills.value.includes(target.value)) {
+      this.usedSkills.push(new FormControl(target.value));
+    }
+  }
+
+  addRequestedSkill(event: Event) {
+    const target = event.target as HTMLSelectElement;
+    if (target && target.value && !this.requestedSkills.value.includes(target.value)) {
+      this.requestedSkills.push(new FormControl(target.value));
+    }
+  }
+
+  removeUsedSkill(index: number) {
+    this.usedSkills.removeAt(index);
+  }
+
+  removeRequestedSkill(index: number) {
+    this.requestedSkills.removeAt(index);
+  }
+
   onSubmit() {
     if (this.myForm.valid) {
       console.log(this.myForm.value);
-      //      const user: User = this.myForm.value;
-      //      console.log(user);
     } else {
       console.log('Form is invalid');
     }
   }
 
-  // Méthode pour écouter les changements de valeurs du formulaire
   ecouter() {
     this.myForm.valueChanges.subscribe((value) => {
-      //console.log(value);
+      // console.log(value);
     });
   }
 
@@ -53,17 +79,18 @@ export class CreateProjectComponent {
       nameProject: user.nameProject,
       nameOwner: user.nameOwner,
       genre: user.genre,
+      usedSkills: user.usedSkills,
+      requestedSkills: user.requestedSkills,
       description: user.description,
-      date: user.date,
     });
   }
 }
 
-// Assurez-vous que la classe User est définie quelque part dans votre code, par exemple :
 interface User {
   nameProject: string;
   nameOwner: string;
   genre: string;
+  usedSkills: string[];
+  requestedSkills: string[];
   description: string;
-  date: string;
 }
