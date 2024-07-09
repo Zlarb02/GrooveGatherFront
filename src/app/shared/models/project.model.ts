39a90c8aacf4f26778a56f07748d449e24a0bcea
StanislasComputerScience <remilabonne@yahoo.fr>
2024-07-09 23:59:09 +0200

    on recommance le post

--- src/app/shared/models/project.model.ts
+++ src/app/shared/models/project.model.ts
@@ -1,14 +1,14 @@
-import type { Genre } from "./genre";
-import type { skillName } from "./skill-name";
+import type { Genre } from './genre';
+import type { skillName } from './skill-name';
 
 export interface Project {
-    id: number;
-    name: string;
-    genres: Genre[];
-    color: string;
-    description: string;
-    date: string;
-    likes: number;
-    skillsPresent: skillName[];
-    skillsMissing: skillName[];
+  id: number | null;
+  name: string;
+  genres: Genre[];
+  color: string;
+  description: string;
+  date: string;
+  likes: number;
+  skillsPresent: skillName[];
+  skillsMissing: skillName[];
 }
